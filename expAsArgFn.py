557186e1bcf8576fdb2cddf70a35a977696055de
Daniel Rojas <daniel.drojas@outlook.com>
2021-04-27 16:47:47 -0500

    use print as a function obj

--- expAsArgFn.py
+++ expAsArgFn.py
@@ -1,3 +1,5 @@
+from __future__ import print_function, division
+
 def do_twice(f, value):
     f(value)
     f(value)
@@ -6,6 +8,14 @@ def print_twice(bruce):
     print(bruce)
     print(bruce)
 
+def do_four(f, value):
+    do_twice(f, value)
+    do_twice(f,value)
 
-do_twice(print_twice, 'spam')
+  
+
+do_four(print,  "spam")
+
+
+
 
